061202407a2fafbfab4edf8fa807dc09d0ce0e33
RetrozDev <ryanbarbozapro@gmail.com>
2024-06-15 00:43:41 +0200

    Ajout de la page Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Home from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>I&apos;m Home</h1>
+    </div>
+  );
+};
+export default Home;
