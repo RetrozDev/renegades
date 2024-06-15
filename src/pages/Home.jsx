68d619449d6e16e62f98eae4d87b72c2fcdaf21e
RetrozDev <ryanbarbozapro@gmail.com>
2024-06-15 03:14:21 +0200

    Amelioration css trailer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,33 @@
+import { useRef, useEffect } from "react";
 import "../styles/Home/Home.css";
 import trailer from "../assets/trailer.mp4";
 
-import ReactPlayer from "react-player";
-
 import HomeHeader from "../components/Home/HomeHeader";
 import Home1stPar from "../components/Home/Home1stPar";
+
 const Home = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.volume = 0.05;
+    }
+  }, []);
+
   return (
     <div className="Home">
       <HomeHeader />
       <Home1stPar />
-      <ReactPlayer
-        url={trailer}
-        controls={true}
-        className="trailerPlayer"
-        volume={0.1}
-        width={"auto"}
-        height={"auto"}
+      <video
+        ref={videoRef}
+        src={trailer}
+        preload="auto"
+        style={{ aspectRatio: "auto" }}
+        className="homeTrailer"
+        controls
       />
     </div>
   );
 };
+
 export default Home;
