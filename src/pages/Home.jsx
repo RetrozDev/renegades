88baa94699c03665a1ae30fc15b038d806de6f11
RetrozDev <ryanbarbozapro@gmail.com>
2024-06-15 02:15:37 +0200

    ajout du trailer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,16 @@ const Home = () => {
     <div className="Home">
       <HomeHeader />
       <Home1stPar />
+      <div className="trailerContainer">
+        <iframe
+          src="https://www.youtube.com/embed/O9N76XGdaEA?rel=0&modestbranding=0&controls=1&rel=0&playsinline=1&enablejsapi=1"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
+      </div>
     </div>
   );
 };
